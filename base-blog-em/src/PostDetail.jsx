e1c1da1164bc572f3457b037fe7bf7dd22bcd385
seungchan <superfly09@daum.net>
2024-08-27 16:40:43 +0900

    feat: edit mutation

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchComments } from "./api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { fetchComments, updatePost } from "./api";
 import "./PostDetail.css";
 
 export function PostDetail({ post }) {
@@ -7,6 +7,20 @@ export function PostDetail({ post }) {
     queryKey: ["detail", post.id],
     queryFn: () => fetchComments(post.id),
   });
+
+  const {
+    data: mutateData,
+    mutate,
+    isSuccess,
+    submittedAt,
+  } = useMutation({
+    mutationFn: (postId) => updatePost(postId),
+  });
+
+  if (isSuccess) {
+    console.log("[submittedAt]:", submittedAt);
+    console.log("mutation success", mutateData);
+  }
 
   if (isLoading) {
     return <>Loading...</>;
@@ -19,7 +33,18 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button>Delete</button>{" "}
+      <button
+        onClick={() =>
+          mutate(post.id, {
+            onSuccess: (result) => {
+              console.log("[onSuccess]", result);
+            },
+          })
+        }
+      >
+        Update title
+      </button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
